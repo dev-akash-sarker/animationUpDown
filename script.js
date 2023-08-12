eb8493f48421ff76655820b51cba198612d1ec18
dev-akash-sarker <akashsarker210@gmail.com>
2023-08-12 19:35:42 +0600

    ssll

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(window).scroll(function () {
-  if ($(this).scrollTop() > 10) {
+  if ($(this).scrollTop() > 230) {
     $("#face-placeholder").addClass("newClass");
     $("#face-placeholder-two").addClass("newClassTwo");
   } else {
     $("#face-placeholder").removeClass("newClass");
     $("#face-placeholder-two").removeClass("newClassTwo");
+  }
+  if ($(this).scrollTop() > 250) {
+    $(".newClassTwo").addClass("newClassTwoMax");
+  } else {
+    $(".newClassTwo").removeClass("newClassTwoMax");
   }
 });
 // $(window).scroll(function () {
