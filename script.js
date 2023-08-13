eaac73d0d866a9192e3ff05605c69c60fc1c3fff
dev-akash-sarker <akashsarker210@gmail.com>
2023-08-14 02:06:09 +0600

    done

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 230) {
-    $("#face-placeholder").addClass("newClass");
-    $("#face-placeholder-two").addClass("newClassTwo");
+const handleScroll = () => {
+  const targetNumber = 230;
+  const scrollPosition = window.scrollY;
+  const elementOne = document.getElementById("face-placeholder");
+  const elementTwo = document.getElementById("face-placeholder-two");
+  if (scrollPosition > targetNumber) {
+    elementOne.style.transform = "translate(-350px, 350px) scale(2)";
+    elementTwo.style.transform = "translate(100px, 1000px) scale(2)";
   } else {
-    $("#face-placeholder").removeClass("newClass");
-    $("#face-placeholder-two").removeClass("newClassTwo");
+    elementOne.style.transform = "translate(0px, 0px) scale(1)";
+    elementTwo.style.transform = "translate(0px, 0px) scale(1)";
   }
-});
-// $(window).scroll(function () {
-//   if ($(this).scrollTop() > 10) {
-//     $("#face-placeholder").addClass("newClass");
-//   } else {
-//     $("#face-placeholder").removeClass("newClass");
-//   }
-// });
+};
 
-// const handleScroll = () => {
-//   const thresold = 230;
-//   const scrollPosition = window.scrollY;
-//   const elementOne = document.getElementById("face-placeholder");
-//   const elementTwo = document.getElementById("face-placeholder-two");
-//   if (scrollPosition > thresold) {
-//     elementOne.classList.add("newClass");
-//     elementTwo.classList.add("newClassTwo");
-//   } else {
-//     elementOne.classList.remove("newClass");
-//     elementTwo.classList.remove("newClassTwo");
-//   }
-// };
-
-// window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handleScroll);
